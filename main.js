155013f443dee405d3d865e5fdfef187fbd4c404
mopsiq <60403947+mopsiq@users.noreply.github.com>
2020-12-20 23:49:42 +0200

    Third commit

--- main.js
+++ main.js
@@ -1,35 +1,66 @@
 "use strict";
 
-let block = document.querySelectorAll('.block');
-let blockList = document.querySelectorAll('.block__item');
-let orderList = document.querySelector('.orders');
-let btn = document.querySelector('.block__btn');
-let test = {
-  name: 'Jack',
-  age: 15
-}
-let getStorage = window.localStorage.setItem('user', JSON.stringify(test));
+const block = document.querySelectorAll('.block__inner');
+const blockList = document.querySelectorAll('.block__item');
+const orderList = document.querySelector('.orders');
+const btn = document.querySelectorAll('.block__btn');
+const btnTwo = document.querySelector('.order__btn');
 
 
-function getValueBlocks(item) {
-  let count = [];
-    for(let i = 0; i < item.children.length; i++) {
-        count.push(item.children[i].getAttribute('order-state'));
-    }
-  return count.map(i => +i);
+function getStyle(className) {
+    let variable = '';
+    let classes = document.styleSheets[0].rules || document.styleSheets[0].cssRules;
+      for (let i = 0; i < classes.length; i++) {
+
+          if (classes[i].selectorText == className) {
+              (classes[i].cssText) ? variable += classes[i].cssText : variable += classes[i].style.cssText;
+          }
+          
+      }
+
+    return variable
 }
+
+function getDataInStorage(item) {
+    let obj = getStyle('.' + item.className);
+    console.log(item.attributes)
+    window.localStorage.setItem('user', JSON.stringify(obj))
+    console.log(obj)
+}
+
 
 document.addEventListener('DOMContentLoaded', (e) => {
-  let storage = window.localStorage.getItem('user')
-  let newObj = JSON.parse(storage)
-  console.log(newObj.name)
 
-  btn.addEventListener('click', (e) => {
+// let getStorage = window.localStorage.setItem('user', JSON.stringify(obj));
+// let storage = window.localStorage.getItem('user')
+// let newObj = JSON.parse(storage)
+// let newObj;
 
-      block.forEach((item) => {
-          console.log(getValueBlocks(item))
-      })
+    if(btn) {
+        
+        btn.forEach((item) => {
+          let currentBlock = item.parentElement.parentElement;
+            item.addEventListener('click', () =>  {
+                if(!currentBlock.classList.contains('hidden')) {
+                  getDataInStorage(currentBlock)
+                  currentBlock.classList.add('hidden')
+                  currentBlock.setAttribute('disabled', 'disabled')
+                } 
 
-  })
+            })
+        })
+
+    } 
+    
+    if(btnTwo) {
+
+        btnTwo.addEventListener('click', () => {
+          let storage = window.localStorage.getItem('user');
+          console.log(JSON.parse(storage))
+        })
+
+    }
+
 })
-// console.log(document.querySelector('.block__item').getAttribute('order-state'));
+
+
